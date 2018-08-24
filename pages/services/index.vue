39a0b75b592763478dd8b5eb51926b75d49548bc
root <irreferential@gmail.com>
2018-08-24 00:50:22 -0400

    Adds service pages

---
+++ pages/services/index.vue
@@ -0,0 +1,34 @@
+<template>
+
+    <Services />
+
+</template>
+
+<script>
+export default {
+    async fetch({ store }) {
+      if(store.state.callsToAction.length === 0 ) {
+        await store.dispatch('getCallsToAction');
+      }
+
+      if (store.state.events.length === 0) {
+        await store.dispatch('getUpcomingEvents');
+      }
+
+      if (store.state.services.length === 0) {
+        await store.dispatch("getServices");
+      }
+
+      if (store.state.pages.length === 0) {
+        await store.dispatch("getPages");
+      }
+      if (store.state.collection.length === 0) {
+        await store.dispatch("getCollection");
+      }
+
+      if (store.state.posts.length === 0) {
+        await store.dispatch("getPosts");
+      }
+    }
+};
+</script>
