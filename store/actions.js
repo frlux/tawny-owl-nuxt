c7660b947d036f27b821f3c33c89747bff251992
awestlib <awest@fontanalib.org>
2019-02-05 16:56:38 -0500

    add ? to urls to prep queries

--- store/actions.js
+++ store/actions.js
@@ -1,17 +1,17 @@
 import axios from "axios/index";
 
 const urls = {
-  authors: 'https://fontana.librarians.design/wp-json/wp/v2/users',
-  callsToAction: 'https://fontana.librarians.design/wp-json/wp/v2/calls-to-action',
-  collection: 'https://fontana.librarians.design/wp-json/wp/v2/collection?_embed',
-  featuredCollections: 'https://fontana.librarians.design/wp-json/wp/v2/featured-collections',
-  locations: 'https://fontana.librarians.design/wp-json/wp/v2/locations',
-  pages: 'https://fontana.librarians.design/wp-json/wp/v2/pages',
+  authors: 'https://fontana.librarians.design/wp-json/wp/v2/users?',
+  callsToAction: 'https://fontana.librarians.design/wp-json/wp/v2/calls-to-action?',
+  collection: 'https://fontana.librarians.design/wp-json/wp/v2/collection?',
+  featuredCollections: 'https://fontana.librarians.design/wp-json/wp/v2/featured-collections?',
+  locations: 'https://fontana.librarians.design/wp-json/wp/v2/locations?',
+  pages: 'https://fontana.librarians.design/wp-json/wp/v2/pages?',
   posts: 'https://public-api.wordpress.com/rest/v1.1/sites/fontanalib.wordpress.com/posts/?number=10',
-  articles: 'https://fontana.librarians.design/wp-json/wp/v2/posts',
-  resources: 'https://fontana.librarians.design/wp-json/wp/v2/resources',
+  articles: 'https://fontana.librarians.design/wp-json/wp/v2/posts?',
+  resources: 'https://fontana.librarians.design/wp-json/wp/v2/resources?',
   services: 'https://fontana.librarians.design/wp-json/wp/v2/services?per_page=50',
-  events: 'https://fontana.librarians.design/wp-json/wp/v2/events',
+  events: 'https://fontana.librarians.design/wp-json/wp/v2/events?',
 };
 
 const actions = {
--- store/index.js
+++ store/index.js
@@ -2,7 +2,19 @@ import Vuex from 'vuex';
 import axios from 'axios';
 import actions from './actions';
 import mutations from './mutations';
-
+const urls = {
+  authors: 'https://fontana.librarians.design/wp-json/wp/v2/users?',
+  callsToAction: 'https://fontana.librarians.design/wp-json/wp/v2/calls-to-action?',
+  collection: 'https://fontana.librarians.design/wp-json/wp/v2/collection?',
+  featuredCollections: 'https://fontana.librarians.design/wp-json/wp/v2/featured-collections?',
+  locations: 'https://fontana.librarians.design/wp-json/wp/v2/locations?',
+  pages: 'https://fontana.librarians.design/wp-json/wp/v2/pages?',
+  posts: 'https://public-api.wordpress.com/rest/v1.1/sites/fontanalib.wordpress.com/posts/?number=10',
+  articles: 'https://fontana.librarians.design/wp-json/wp/v2/posts?',
+  resources: 'https://fontana.librarians.design/wp-json/wp/v2/resources?',
+  services: 'https://fontana.librarians.design/wp-json/wp/v2/services?per_page=50',
+  events: 'https://fontana.librarians.design/wp-json/wp/v2/events?',
+};
 const createStore = () => {
   return new Vuex.Store({
     state: {
